8c65a6c695080bfe058fb64705e6807be4066e82
Sirotkin Sergey <sirotkinsa@student.bmstu.ru>
2025-02-23 17:48:07 +0300

    add

--- internal/models/restaurant.go
+++ internal/models/restaurant.go
@@ -3,9 +3,9 @@ package models
 import uuid "github.com/satori/uuid"
 
 type Restaurant struct {
-	Id           uuid.UUID `json:"id"`
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	Type        string  `json:"type"`
-	Rating      float64 `json:"rating"`
+	Id          uuid.UUID `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Type        string    `json:"type"`
+	Rating      float64   `json:"rating"`
 }
