5536fe8c955808565e82e9eea0ef98eebffb1e92
Sirotkin Sergey <sirotkinsa@student.bmstu.ru>
2025-04-03 02:56:09 +0300

    del trash

--- internal/models/cart_item.go
+++ internal/models/cart_item.go
@@ -2,9 +2,9 @@ package models
 
 // easyjson:json
 type CartItem struct {
-    Name     string  `json:"name"`
-    Price    float64 `json:"price"`
-    ImageURL string  `json:"image_url"`
-    Weight   float64 `json:"weight"`
-    Amount   int     `json:"amount"`
+	Name     string  `json:"name"`
+	Price    float64 `json:"price"`
+	ImageURL string  `json:"image_url"`
+	Weight   float64 `json:"weight"`
+	Amount   int     `json:"amount"`
 }
