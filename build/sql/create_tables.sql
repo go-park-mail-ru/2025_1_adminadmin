21e26cf22ef6f1dbdf4e049dae76f06dea02339f
Nikita <nikvop05@mail.ru>
2025-04-03 22:48:06 +0300

    fix

--- build/sql/create_tables.sql
+++ build/sql/create_tables.sql
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS orders (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     status TEXT NOT NULL,
-    address_id UUID NOT NULL REFERENCES address(id) ON DELETE CASCADE,
+    address_id UUID NOT NULL REFERENCES addresses(id) ON DELETE CASCADE,
     order_products TEXT NOT NULL
 );
 
