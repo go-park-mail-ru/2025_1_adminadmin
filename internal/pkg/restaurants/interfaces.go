868c0d06b89b642ea5994f38b173ce2deb8645b8
totorialman <workbook2003@gmail.com>
2025-04-16 01:00:10 +0300

    fix

--- internal/pkg/restaurants/interfaces.go
+++ internal/pkg/restaurants/interfaces.go
@@ -9,12 +9,10 @@ import (
 
 type RestaurantRepo interface {
 	GetAll(ctx context.Context, count, offset int) ([]models.Restaurant, error)
-	GetById(ctx context.Context, id uuid.UUID) (*models.Restaurant, error)
 	GetProductsByRestaurant(ctx context.Context, restaurantID uuid.UUID, count, offset int) (*models.RestaurantFull, error)
 }
 
 type RestaurantUsecase interface {
 	GetAll(ctx context.Context, count, offset int) ([]models.Restaurant, error)
-	GetById(ctx context.Context, id uuid.UUID) (*models.Restaurant, error)
 	GetProductsByRestaurant(ctx context.Context, restaurantID uuid.UUID, count, offset int) (*models.RestaurantFull, error)
 }
