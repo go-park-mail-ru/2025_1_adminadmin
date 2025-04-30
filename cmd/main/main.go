9ee4559250cf3ce400dc969cd59ed78f442ef938
Nikita <nikvop05@mail.ru>
2025-04-30 14:21:44 +0300

    add test payment handler

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -134,6 +134,7 @@ func main() {
 		order.HandleFunc("/create", cartHandler.CreateOrder).Methods(http.MethodPost, http.MethodOptions)
 	}
 	
+	r.HandleFunc("/payment", cartHandler.Payment).Methods(http.MethodPost)
 	http.Handle("/", r)
 	srv := http.Server{
 		Handler:           r,
