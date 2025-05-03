491ea23771f2bd71b2f0e02e8eb9cc77ae4c4399
Nikita <nikvop05@mail.ru>
2025-05-04 02:09:00 +0300

    fix cart

--- cmd/cart/main.go
+++ cmd/cart/main.go
@@ -3,10 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
-	"io"
-	"log/slog"
 	"net"
-	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -16,9 +13,7 @@ import (
 	cartPgRepo "github.com/go-park-mail-ru/2025_1_adminadmin/internal/pkg/cart/repo/pg"
 	cartRedisRepo "github.com/go-park-mail-ru/2025_1_adminadmin/internal/pkg/cart/repo/redis"
 	cartUsecase "github.com/go-park-mail-ru/2025_1_adminadmin/internal/pkg/cart/usecase"
-	"github.com/gorilla/mux"
 	"github.com/jackc/pgx/v4/pgxpool"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/redis/go-redis/v9"
 	"google.golang.org/grpc"
 )
@@ -39,13 +34,6 @@ func main() {
 }
 
 func run() (err error) {
-	logFile, err := os.OpenFile(os.Getenv("AUTH_LOG_FILE"), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	if err != nil {
-		fmt.Println("error opening log file: " + err.Error())
-		return
-	}
-	defer logFile.Close()
-	logger := slog.New(slog.NewJSONHandler(io.MultiWriter(logFile, os.Stdout), &slog.HandlerOptions{Level: slog.LevelInfo}))
 
 	db, err := pgxpool.Connect(context.Background(), os.Getenv("POSTGRES_CONN"))
 	if err != nil {
@@ -65,18 +53,9 @@ func run() (err error) {
 	CartUsecase := cartUsecase.NewCartUsecase(cartRepoRedis, CartRepoPg)
 	CartDelivery := grpcCart.CreateCartHandler(CartUsecase)
 
-	//grpcMetrics, err := metrics.NewGrpcMetrics("cart")
-	if err != nil {
-		logger.Error("can't create metrics")
-	}
-	//metricsMw := metricsmw.NewGrpcMw(*grpcMetrics)
-
 	gRPCServer := grpc.NewServer()
 	generatedCart.RegisterCartServiceServer(gRPCServer, CartDelivery)
 
-	r := mux.NewRouter().PathPrefix("/api").Subrouter()
-	r.PathPrefix("/metrics").Handler(promhttp.Handler())
-	http.Handle("/", r)
 	go func() {
 		listener, err := net.Listen("tcp", fmt.Sprintf(":%s", "5460"))
 		if err != nil {
